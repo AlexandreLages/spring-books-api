f8bc6977cecc27448fc979d9584f575035e5941e
alexandrelages <alexandrelages64@gmail.com>
2023-05-23 20:27:31 -0300

    Commit inicial

---
+++ src/test/java/com/alexandrelages/springbooksapi/SpringBooksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandrelages.springbooksapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBooksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
